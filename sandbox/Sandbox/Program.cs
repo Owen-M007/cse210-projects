c6eb57ef6a0cd18e97dc17ed7136c65d81e82dee
Owen-M007 <owenjmeans@gmail.com>
2026-04-27 15:00:55 -0600

    did more stuff

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,6 +6,34 @@ class Program
     {
         // this is a super duper epic and cool comment wowza
         // another comment
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello Sandbox!");
+
+        int x = 10;
+
+        if (x == 11)
+        {
+            Console.WriteLine("Hey Bob");
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        int age = -1;
+
+        while (age < 0 || age > 125)
+        {
+            Console.WriteLine("Please enter your age: ");
+            age = int.Parse(Console.ReadLine());
+        }
+
+        int age2;
+
+        do
+        {
+            Console.WriteLine("Please enter your age: ");
+            age2 = int.Parse(Console.ReadLine());
+        } while (age2 < 0 || age > 125);
     }
 }
