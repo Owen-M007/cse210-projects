afbbb629feb6af1886b7dfa925218c3f93624e1d
Owen-M007 <owenjmeans@gmail.com>
2026-04-29 17:14:03 -0600

    first version

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magicNum = int.Parse(Console.ReadLine());
+        
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        if (guess > magicNum)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < magicNum)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
