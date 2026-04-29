b7348735d829cbaa3d429296982f20b523d43092
Owen-M007 <owenjmeans@gmail.com>
2026-04-29 17:33:35 -0600

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,14 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        int magicNum = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 100);
 
         int guess = -1;
 
         while (guess != magicNum)
         {
-            Console.Write("What is your guess? ");
+            Console.Write("What is the magic number? ");
             guess = int.Parse(Console.ReadLine());
 
             if (guess > magicNum)
