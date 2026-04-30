4cef2d7f63cda3918d8c10f9cce99c9d7ff2fb71
Owen-M007 <owenjmeans@gmail.com>
2026-04-29 18:50:06 -0600

    first version

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        List<int> nums = new List<int>();
+
+        int num;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            num = int.Parse(Console.ReadLine());
+            nums.Add(num);
+        } while (num != 0);
+
+        for (int i = 0; i < nums.Count; i++)
+        {
+            Console.WriteLine(nums[i]);
+        }
+
     }
 }
