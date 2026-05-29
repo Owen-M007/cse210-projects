150b0722ceb618b53ac5fe44890e57da1c74e90c
Owen-M007 <owenjmeans@gmail.com>
2026-05-28 22:06:01 -0600

    Got Reference class working

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,17 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Word myWord = new Word("Hello");
-        Console.WriteLine(myWord.GetWordString());
+        // Word myWord = new Word("Hello");
+        // Console.WriteLine(myWord.GetWordString());
 
-        myWord.Hide();
-        Console.WriteLine(myWord.GetWordString());
+        // myWord.Hide();
+        // Console.WriteLine(myWord.GetWordString());
 
-        myWord.DisplayWord();
-        if (myWord.IsHidden())
-        {
-            Console.WriteLine(myWord.GetWordString());
-        }
+        // myWord.DisplayWord();
+        // if (myWord.IsHidden())
+        // {
+        //     Console.WriteLine(myWord.GetWordString());
+        // }
 
+        Reference myReference = new Reference("1 Nephi", 2, 8);
+        myReference.ShowReference();
     }
 }
--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,4 +1,57 @@
 class Reference
 {
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+    private bool _multipleVerse;
+
+    public Reference(string Book, int Chapter, int Verse)
+    {
+        _book = Book;
+        _chapter = Chapter;
+        _verse = Verse;
+        _multipleVerse = false;
+    }
+
+    public Reference(string Book, int Chapter, int Verse, int EndVerse)
+    {
+        _book = Book;
+        _chapter = Chapter;
+        _verse = Verse;
+        _endVerse = EndVerse;
+        _multipleVerse = true;
+    }
+
+    // public string GetReference()
+    // {
+        
+    // }
+
+
+    // keeping this public for now, may change to private if I end up using the commented out method above
+    public string GetReferenceString()
+    {
+        string referenceString;
+        
+        if(_multipleVerse == false)
+        {
+            referenceString = $"{_book} {_chapter}:{_verse}";
+        }
+        else
+        {
+            referenceString = $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
+
+        return referenceString;
+    }
+
+    public void ShowReference()
+    {
+        Console.WriteLine(GetReferenceString());
+    }
+
     
+
+
 }
