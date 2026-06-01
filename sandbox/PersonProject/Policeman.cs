afed69811b32fc64019300b0272eace78faddde0
Owen-M007 <owenjmeans@gmail.com>
2026-06-01 15:41:39 -0600

    created Person class and Policeman class for in-class activity

---
+++ sandbox/PersonProject/Policeman.cs
@@ -0,0 +1,7 @@
+class Policeman : Person
+{
+    public Policeman(string weapons, string firstName, string lastName, int age, int weight) : base(firstName, lastName, age, weight)
+    {
+        
+    }
+}
---
+++ sandbox/PersonProject/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("helloz");
+
+        Person myPerson = new Person("Bob", "Roberts", 57, 195);
+        Console.WriteLine(myPerson.GetPersonInfo());
+    }
+}
