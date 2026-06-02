813fc0fd5381f5814573c78bc244a87d6ac7b234
Owen-M007 <owenjmeans@gmail.com>
2026-06-02 16:03:40 -0600

    Completed Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -3,7 +3,16 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+    {   
+        Assignment myAssignment = new Assignment("Owen Means", "Physics");
+        Console.WriteLine(myAssignment.GetSummary());
+
+        MathAssignment myMathAssignment = new MathAssignment("7.3", "8-19", "Owen Means", "Math");
+        Console.WriteLine(myMathAssignment.GetHomeworkList());
+        Console.WriteLine(myMathAssignment.GetSummary());
+
+        WritingAssignment myWritingAssignment = new WritingAssignment("Writing Is Epic", "Owen Means", "English 310");
+        Console.WriteLine(myWritingAssignment.GetWritingInfo());
+        Console.WriteLine(myWritingAssignment.GetSummary());
     }
 }
