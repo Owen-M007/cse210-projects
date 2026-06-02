2fdd8e36a0ac6275a155d5256a1b56b3286740b9
Owen-M007 <owenjmeans@gmail.com>
2026-06-02 16:05:10 -0600

    Working on Scripture class

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,22 +1,35 @@
 class Scripture
 {
-    private List<Word> _words;
+    private List<String> _words;
     private Reference _reference;
 
-    public Scripture(string Book, int Chapter, int Verse, string Text)
-    {
+    // public Scripture(string Book, int Chapter, int Verse, string Text)
+    // {
         
-    }
+    // }
 
-    public Scripture(string Book, int Chapter, int Verse, int EndVerse, string Text)
-    {
+    // public Scripture(string Book, int Chapter, int Verse, int EndVerse, string Text)
+    // {
         
-    }
+    // }
 
     public Scripture(Reference Ref, string Text)
     {
-        _reference = Ref.GetReferenceString()
+        _reference = Ref;
+        foreach(string Word in Text.Split(" "))
+        {
+            _words.Add(Word);
+        }
+    }
 
+    public void RandomWordPicker()
+    {
+        Random rand = new Random();
+        
+        for(int i = 0; i < 3; i++)
+        {
+            int RandomIndex = rand.Next(_words.Count);
+        }
     }
 
     // private List<Word> ConvertToWords(string Text)
