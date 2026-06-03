d90fb21f4ef3216863a9adbfdfa587140206f7c0
Owen-M007 <owenjmeans@gmail.com>
2026-06-02 18:27:17 -0600

    version 1 of user input and interaction

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -23,9 +23,23 @@ class Program
         // myReference2.ShowReference();
 
         Scripture myScripture1 = new Scripture(myReference1, "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.");
-        myScripture1.ShowScripture();
+        // myScripture1.ShowScripture();
 
         Scripture myScripture2 = new Scripture(myReference2, "And when ye shall receive these things, I would exhort you that ye would ask God, the Eternal Father, in the name of Christ, if these things are not true; and if ye shall ask with a sincere heart, with real intent, having faith in Christ, he will manifest the truth of it unto you, by the power of the Holy Ghost. And by the power of the Holy Ghost ye may know the truth of all things.");
-        myScripture2.ShowScripture();
+        // myScripture2.ShowScripture();
+
+        Console.WriteLine("Welcome to the Scripture Memorizer! (patent pending (totally (for real)))");
+        Console.WriteLine("Press Enter to initiate.");
+        Console.WriteLine("> ");
+        string userInput = "";
+        do
+        {
+            userInput = Console.ReadLine();
+            if(userInput != "q")
+            {
+            Console.WriteLine("Blah blah blah");
+            }
+        } while(userInput != "q");
+
     }
 }
