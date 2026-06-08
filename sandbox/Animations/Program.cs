88d4770e26b4194ebd1054cf1bd447280a52eb4b
Owen-M007 <owenjmeans@gmail.com>
2026-06-08 15:46:59 -0600

    Made Animations class

---
+++ sandbox/Animations/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int sleepTime = 250;
+        
+        Console.Clear();
+        Console.CursorVisible = false;
+        for (int i = 0; i< 20; i++)
+        {
+            Console.Write("+");
+            Thread.Sleep(sleepTime);
+            Console.Write("\b");
+            Console.Write("-");
+            Thread.Sleep(sleepTime);
+            Console.Write("\b");
+        }
+
+
+        string animationString = "|\\-/";
+        for (int i = 0; i< 20; i++)
+        {
+            Console.Write(animationString[i % animationString.Length]);
+            Thread.Sleep(sleepTime);
+            Console.Write("\b");
+        }
+         Console.CursorVisible = true;
+    }
+}
