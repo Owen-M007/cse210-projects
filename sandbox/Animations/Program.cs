7b76c27aadf0ba05a2a6a39a8429e85ee323b450
Owen-M007 <owenjmeans@gmail.com>
2026-06-10 14:25:53 -0600

    Animation and timer stuff in sandbox

--- sandbox/Animations/Program.cs
+++ sandbox/Animations/Program.cs
@@ -6,24 +6,47 @@
         
         Console.Clear();
         Console.CursorVisible = false;
-        for (int i = 0; i< 20; i++)
-        {
-            Console.Write("+");
-            Thread.Sleep(sleepTime);
-            Console.Write("\b");
-            Console.Write("-");
-            Thread.Sleep(sleepTime);
-            Console.Write("\b");
-        }
+        // for (int i = 0; i< 20; i++)
+        // {
+        //     Console.Write("+");
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b");
+        //     Console.Write("-");
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b");
+        // }
 
 
-        string animationString = "|\\-/";
-        for (int i = 0; i< 20; i++)
+        // string animationString = "/-\\|"; 
+        // for (int i = 0; i< 30; i++)
+        // {
+        //     Console.Write(animationString[i % animationString.Length]);
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b");
+        // }
+
+
+        int count = 10;
+        int index = 0;
+
+        // DateTime now = DateTime.Now;
+        // DateTime endTime = now.AddSeconds(count);
+        // while (DateTime.Now < endTime)
+        // {
+        //     Console.Write(animationString[index++ % animationString.Length]);
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b");
+        // }
+
+        
+        // while (DateTime.Now < endTime)
+        while(count >= 0)
         {
-            Console.Write(animationString[i % animationString.Length]);
-            Thread.Sleep(sleepTime);
-            Console.Write("\b");
+            Console.Write($"{count--, 2}");
+            Thread.Sleep(1000);
+            Console.Write("\b\b");
         }
+
          Console.CursorVisible = true;
     }
 }
