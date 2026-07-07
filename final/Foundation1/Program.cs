4aa1cc98c3cf9ce6728b0a1b760fbf180e85614a
Owen-M007 <owenjmeans@gmail.com>
2026-07-06 19:08:58 -0600

    Completed Foundation1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -2,6 +2,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
-    }
+        // Video 1
+        Video video1 = new Video("Classing With Programs!? A Sharp C Tutorial", "Billy Bart", 1200);
+
+        Comment comment1_1 = new Comment("EpicCoder123", "It was totally epic when he abstracted the encapsulation, it made me want to inherit some polymorphism.");
+        Comment comment1_2 = new Comment("Dude", "Wow.");
+        Comment comment1_3 = new Comment("Nuh-Uh", "I Disagree with everything you said, 0/10 tutorial.");
+
+        video1.AddComment(comment1_1);
+        video1.AddComment(comment1_2);
+        video1.AddComment(comment1_3);
+
+        // Video 2
+        Video video2 = new Video("Top 10 Best Dad Jokes (Professional Opinion)", "Bro. Godderidge", 900);
+
+        Comment comment2_1 = new Comment("CSE210 Student 1", "ha ha!");
+        Comment comment2_2 = new Comment("CSE210 Student 2", "tee hee!");
+        Comment comment2_3 = new Comment("CSE210 Student 3", "C'est tres amusant.");
+
+        video2.AddComment(comment2_1);
+        video2.AddComment(comment2_2);
+        video2.AddComment(comment2_3);
+
+        // Video 3
+        Video video3 = new Video("Proving That This Guy Gets It (Gone Wrong)", "Nick Rapp", 7200);
+
+        Comment comment3_1 = new Comment("Certified Man of Great Knowledge", "With my great knowledge, I must say that this guy does, in fact, get it.");
+        Comment comment3_2 = new Comment("Yet Another Guy", "I think I also get it.");
+        Comment comment3_3 = new Comment("Yug", "I don't get it.");
+
+        video3.AddComment(comment3_1);
+        video3.AddComment(comment3_2);
+        video3.AddComment(comment3_3);
+
+        // Display everything
+        List<Video> playlist = [video1, video2, video3];
+        foreach (Video video in playlist)
+        {
+            video.DisplayVideo();
+        }
+        
+    }        
 }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -3,6 +3,34 @@ class Video
     private string _title;
     private string _author;
     private int _length;
+    List<Comment> _comments = new List<Comment>();
 
-    
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return _comments.Count();
+    }
+
+    public void DisplayVideo()
+    {
+        Console.WriteLine("Video:");
+        Console.WriteLine($" > '{_title}' by {_author} (Length in seconds: {_length})");
+        Console.WriteLine("Comments:");
+        foreach (Comment comment in _comments)
+        {
+            comment.DisplayComment();
+        }
+        Console.WriteLine("");
+    }
 }
