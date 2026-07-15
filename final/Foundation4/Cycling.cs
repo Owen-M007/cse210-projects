c1a18fc9273974124029b243ba6de7bc31056693
Owen-M007 <owenjmeans@gmail.com>
2026-07-15 15:56:28 -0600

    Completed Foundation4

--- final/Foundation4/Cycling.cs
+++ final/Foundation4/Cycling.cs
@@ -1,7 +1,29 @@
-// class Cycling : Activity
-// {
-//     public Cycling(string date, int time, double distance) : base(date, time, distance)
-//     {
-        
-//     }
-// }
+class Cycling : Activity
+{
+    private double _speed;
+    
+    public Cycling(string date, int time, double speed) : base(date, time)
+    {
+        _speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return _speed * 60 / GetTime();
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / GetSpeed();
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} Distance {GetDistance():F1} km, Speed {_speed:F1} kph, Pace: {GetPace():F1} min per km";
+    }
+}
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -3,9 +3,13 @@ class Program
     static void Main(string[] args)
     {
         Activity swimming = new Swimming("07/20/26", 70, 20);
-        Console.WriteLine(swimming.GetSummary());
-
         Activity running = new Running("07/21/26", 60, 1.6);
-        Console.WriteLine(running.GetSummary());
+        Activity cycling = new Cycling("07/22/26", 30, 2);
+
+        List<Activity> activityList = [swimming, running, cycling];
+        foreach (Activity activity in activityList)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
